0a458da0cc62ef1130ffdba8e2d338c8cdc6892c
Cyrill <cyrill.wyss@stud.hslu.ch>
2015-03-23 11:14:25 +0100

    Added Events for longpressed and released to robo

--- Robo/Sources/EventHandler.c
+++ Robo/Sources/EventHandler.c
@@ -12,10 +12,14 @@
 
 void ProcessInitEvet(void);
 void ProcessSW1Event(void);
+void ProcessSW1EventLong(void);
+void ProcessSW1EventReleased(void);
 void ProcessLEDHeartbeatEcent(void);
 
 EventAllocation evtAlloc[] = { { EVNT_INIT, ProcessInitEvet }, {
-		EVNT_SW1_PRESSED, ProcessSW1Event }, {
+		EVNT_SW1_PRESSED, ProcessSW1Event },{
+				EVNT_SW1_LPRESSED, ProcessSW1EventLong },{
+						EVNT_SW1_RELEASED, ProcessSW1EventReleased }, {
 				EVNT_LED_HEARTBEAT, ProcessLEDHeartbeatEcent } }; /*!< Allocation between event type and handler function*/
 
 void EventHandler_HandleEvent(void) {
@@ -34,7 +38,12 @@ void ProcessInitEvet(void) {
 void ProcessSW1Event(void) {
 	CLS1_SendStr("S2 Pressed\r\n",CLS1_GetStdio()->stdOut);
 }
-
+void ProcessSW1EventLong(void){
+	CLS1_SendStr("S2 Long Pressed\r\n",CLS1_GetStdio()->stdOut);
+}
+void ProcessSW1EventReleased(void){
+	CLS1_SendStr("S2 Released\r\n",CLS1_GetStdio()->stdOut);
+}
 void ProcessLEDHeartbeatEcent(void) {
 
 	LED1_On();
